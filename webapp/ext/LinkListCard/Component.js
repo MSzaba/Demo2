af085cf846ff11cdc37cb67c4ad94c8d6229259b
Szabolcs Menyhart <szabolcs.menyhart@sap.com>
2020-03-09 10:28:24 +0000

    Adding quick links section

---
+++ webapp/ext/LinkListCard/Component.js
@@ -0,0 +1,44 @@
+sap.ui.define(["sap/ovp/cards/generic/Component"], function (Component) {
+
+	"use strict";
+
+	/* component for custom card */
+	return Component.extend("tst.demo2.Demo2.ext.LinkListCard.Component", {
+		// use inline declaration instead of component.json to save 1 round trip
+		metadata: {
+			properties: {
+				"contentFragment": {
+					"type": "string",
+					"defaultValue": "tst.demo2.Demo2.ext.LinkListCard.LinkListCard"
+				},
+                "countHeaderFragment": {
+                    "type": "string",
+                    "defaultValue": "sap.ovp.cards.generic.CountHeader"
+                },
+				"footerFragment": {
+					"type": "string",
+					"defaultValue": ""
+				}
+			},
+
+			version: "@version@",
+
+			library: "sap.ovp",
+
+			includes: [],
+
+			dependencies: {
+				libs: ["sap.m"],
+				components: []
+			},
+			config: {},
+			customizing: {
+				"sap.ui.controllerExtensions": {
+					"sap.ovp.cards.generic.Card": {
+						controllerName: "tst.demo2.Demo2.ext.LinkListCard.LinkListCard"
+					}
+				}
+			}
+		}
+	});
+});
---
+++ webapp/ext/LinkListCard/LinkListCard.controller.js
@@ -0,0 +1,22 @@
+(function () {
+    "use strict";
+    /*global sap, jQuery */
+    
+   /*
+   Here you can put Controller code
+   */
+ 
+    sap.ui.controller("tst.demo2.Demo2.ext.LinkListCard.LinkListCard", {
+         
+        onInit: function () {
+             
+        },
+ 
+        onAfterRendering: function () {
+ 
+             
+        },
+ 
+ 
+    });
+})();
